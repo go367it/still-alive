94808c5a3d90632f1d4961a93acf3598154f255a
go367it <satyamkumar3001@gmail.com>
2022-10-09 23:03:17 +0530

    added copu function

--- components/Navbar.js/Navbar.js
+++ components/Navbar.js/Navbar.js
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import Container from "../uiComponents/Container.js";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <Container>
       <div className="w-full flex justify-between place-items-center py-2">
-        <header className=" flex justify-between place-items-center gap-2">
-          <Image src="/Images/ninja.png" alt="logo" width="50" height="50" />
-          <h3 className="text-2xl font-bold">Chota.ninja</h3>
-        </header>
+        <Link href='/'>
+          <header className=" flex justify-between place-items-center gap-2 cursor-pointer">
+            <Image src="/Images/ninja.png" alt="logo" width="50" height="50" />
+            <h3 className="text-2xl font-bold">Chota.ninja</h3>
+          </header>
+        </Link>
         <div>
-          <button className="bg-blue-600 px-4 py-2 rounded-lg text-white cursor-pointer hover:bg-blue-500 transform duration-300">
-            Sign In/Log IN
-          </button>
+          <Link href="/login">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-white cursor-pointer hover:bg-blue-500 transform duration-300">
+              Sign In/Log IN
+            </button>
+          </Link>
         </div>
       </div>
     </Container>
