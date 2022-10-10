d7ba3466d45bbda190027aa1d9f21f088f187c47
go367it <satyamkumar3001@gmail.com>
2022-10-10 16:42:22 +0530

    added google login

---
+++ components/GoogleLogin/GoogleLogin.js
@@ -0,0 +1,43 @@
+import { GoogleLogin } from "react-google-login";
+
+export default function LoginGoogle() {
+  const onGoogleSuccess = (response) => {
+    const access_token = response.tokenId;
+
+    axios({
+      method: "post",
+      url: "https://chota.ninja/user/auth/google",
+      data: {
+        token: response.tokenId,
+      },
+    })
+      .then((res) => {
+        const { user, token } = res.data.data;
+
+        // Save the JWT inside a cookie
+        localStorage.setItem("token", token);
+
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        // throw new Error(err);
+        console.log(err)
+      });
+  };
+  const onGoogleFailure = (err) => {
+    //  console.log(err,99900909);
+    console.log(err)
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId="199477342550-hurgvv8q5ud9lgv9megn131k5b9pvf8c.apps.googleusercontent.com"
+        buttonText="Sign in with Google"
+        onSuccess={onGoogleSuccess}
+        onFailure={onGoogleFailure}
+        className="google-login-button"
+      />
+    </div>
+  );
+}
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import Container from "../../components/uiComponents/Container";
+import LoginGoogle from "../../components/GoogleLogin/GoogleLogin";
 
-export default function Login(){
-    return(
-        <div>
-            <p>Login</p>
+export default function Login() {
+  return (
+    <div>
+      <Container>
+        <div className="w-full flex place-items-center justify-center">
+          <div className="p-4 border border-gray-400 rounded-lg w-full max-w-2xl">
+            <p className="text-center text-2xl font-semibold">Login</p>
+            <LoginGoogle />
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
